b771d8e4340341d719dc56f86bb560784258a82d
SandroB <supp.sandrob@gmail.com>
2013-04-03 21:36:07 +0200

    http passthrough proxy remains open with pending sockets

--- projects/SandroProxyLib/src/org/sandrop/webscarab/plugin/proxy/ConnectionHandler.java
+++ projects/SandroProxyLib/src/org/sandrop/webscarab/plugin/proxy/ConnectionHandler.java
@@ -257,6 +257,7 @@ public class ConnectionHandler implements Runnable {
                 OutputStream os = _sock.getOutputStream();
                 response.skipContentStore(true);
                 response.write(os);
+                SocketForwarder.connect(forwarderName, _sock, response.getSocket());
                 return;
             }
 
