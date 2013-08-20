6e9a524c6ac09c2fbc6a8e11d52ad62e6d63b8e6
SandroB <supp.sandrob@gmail.com>
2013-08-20 22:39:56 +0200

    added needed rules to redirect http/https to proxy

--- projects/SandroProxyPlugin/src/org/sandroproxy/plugin/gui/MainActivity.java
+++ projects/SandroProxyPlugin/src/org/sandroproxy/plugin/gui/MainActivity.java
@@ -1,6 +1,14 @@
 package org.sandroproxy.plugin.gui;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
 
 import org.sandrop.webscarab.model.Preferences;
 import org.sandrop.webscarab.model.StoreException;
@@ -46,11 +54,14 @@ public class MainActivity extends Activity {
     private static String mLogWindowMessage = "";
     
     private static boolean mInitChecked = false;
+    
+    private java.util.logging.Logger logger = java.util.logging.Logger.getLogger(getClass().getName());
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logger.setLevel(Level.FINEST);
         mLogView = (TextView) findViewById(R.id.logView); 
         ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButtonOnOff);
         toggleButton.setOnClickListener(new OnClickListener() {
@@ -65,6 +76,9 @@ public class MainActivity extends Activity {
                         @Override
                         public void run() {
                             Preferences.init(getApplicationContext());
+                            if (isDeviceRooted()){
+                                ipTablesForTransparentProxy(true);
+                            }
                             framework = new Framework(getApplicationContext());
                             setStore(getApplicationContext());
                             Proxy proxy = new Proxy(framework, null, null);
@@ -85,6 +99,9 @@ public class MainActivity extends Activity {
                     {
                         @Override
                         public void run() {
+                            if (isDeviceRooted()){
+                                ipTablesForTransparentProxy(false);
+                            }
                             if (framework != null){
                                 framework.stop();
                             }
@@ -160,6 +177,125 @@ public class MainActivity extends Activity {
         }
     }
     
+    
+    /**
+     * Checks if the device is rooted.
+     * 
+     * @return <code>true</code> if the device is rooted, <code>false</code>
+     * otherwise.
+     */
+    public static boolean isDeviceRooted() {
+      // get from build info
+      String buildTags = android.os.Build.TAGS;
+      if (buildTags != null && buildTags.contains("test-keys")) {
+          return true;
+      }
+      try {
+       // check if /system/app/Superuser.apk is present
+        {
+            File file = new File("/system/app/Superuser.apk");
+            if (file.exists()) {
+              return true;
+            }
+        }
+        // search for some typical locations
+        {
+            String[] suPlaces = { "/sbin/", "/system/bin/", "/system/xbin/", "/data/local/xbin/",
+                    "/data/local/bin/", "/system/sd/xbin/", "/system/bin/failsafe/", "/data/local/" };
+            for (String suPlace : suPlaces) {
+                File file = new File(suPlace + "su");
+                if (file.exists()) {
+                    return true;
+                }
+            }
+        }
+      } catch (Throwable e1) {
+        // ignore
+      }
+      return false;
+    }
+    
+    
+    private void ipTablesForTransparentProxy(boolean activate){
+        int processId = getApplicationInfo().uid;
+        String excludedUid = String.valueOf(processId);
+        String action = "A";
+        String chainName = "spplugin";
+        String chainName1 = "sppluginOutput";
+        List<String> rules = new ArrayList<String>();
+
+        if (activate){
+            action = "A";
+            String createChainRule = "iptables --new " + chainName; rules.add(createChainRule);
+            String createNatChainRule = "iptables -t nat --new " + chainName; rules.add(createNatChainRule);
+            String createNatChainRule1 = "iptables -t nat --new " + chainName1; rules.add(createNatChainRule1);
+        }else{
+            action = "D";
+            String dettachChainRule = "iptables -D INPUT -j " + chainName; rules.add(dettachChainRule);
+            String dettachNatChainRule = "iptables -t nat -D PREROUTING -j " + chainName; rules.add(dettachNatChainRule);
+            String dettachNatChainRule1 = "iptables -t nat -D OUTPUT -j " + chainName1; rules.add(dettachNatChainRule1);
+        }
+        
+        // create 80 rules 
+        String accept80Rule = "iptables -" + action + " " + chainName + " -p tcp --dport 80 -j ACCEPT "; rules.add(accept80Rule);
+        String redirect80Rule = "iptables -" + action + " " + chainName + " -t nat -p tcp --dport 80 -j REDIRECT --to-port 8009 ";rules.add(redirect80Rule);
+        String exclude80UidRule = "iptables -t nat -" + action + " " + chainName1 + " -m owner ! --uid-owner " + excludedUid + " -p tcp --dport 80 -j DNAT --to 127.0.0.1:8009 ";rules.add(exclude80UidRule);
+        
+        // create 443 rules 
+        String accept443Rule = "iptables -" + action + " " + chainName + " -p tcp --dport 443 -j ACCEPT ";rules.add(accept443Rule);
+        String redirect443Rule = "iptables -" + action + " " + chainName + " -t nat -p tcp --dport 443 -j REDIRECT --to-port 8010 ";rules.add(redirect443Rule);
+        String exclude443UidRule = "iptables -t nat -" + action + " " + chainName1 + " -m owner ! --uid-owner " + excludedUid + " -p tcp --dport 443 -j DNAT --to 127.0.0.1:8010 ";rules.add(exclude443UidRule);
+        
+        if (activate){
+            String attachChainRule = "iptables -A INPUT -j " + chainName; rules.add(attachChainRule);
+            String attachNatChainRule = "iptables -t nat -A PREROUTING -j " + chainName; rules.add(attachNatChainRule);
+            String attachNatChainRule1 = "iptables -t nat -A OUTPUT -j " + chainName1; rules.add(attachNatChainRule1);
+        }else{
+            
+            String deleteChainRule = "iptables --delete-chain " + chainName; rules.add(deleteChainRule);
+            String deleteNatChainRule = "iptables -t nat --delete-chain " + chainName; rules.add(deleteNatChainRule);
+            String deleteNatChainRule1 = "iptables -t nat --delete-chain " + chainName1; rules.add(deleteNatChainRule1);
+        }
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(new String[]{"su", "-c", "sh"});
+        
+            DataOutputStream stdin = new DataOutputStream(p.getOutputStream());
+            DataInputStream stdout = new DataInputStream(p.getInputStream());
+            InputStream stderr = p.getErrorStream();
+            
+            for (String rule : rules) {
+                logger.finest(rule);
+                stdin.writeBytes(rule + "\n");
+                stdin.writeBytes("echo $?\n");
+                Thread.sleep(100);
+                byte[] buffer = new byte[4096];
+                int read = 0;
+                String out = new String();
+                String err = new String();
+                while(true){
+                    read = stdout.read(buffer);
+                    out += new String(buffer, 0, read);
+                    if(read<4096){
+                        break;
+                    }
+                }
+                while(stderr.available() > 0){
+                    read = stderr.read(buffer);
+                    err += new String(buffer, 0, read);
+                    if(read < 4096){
+                        break;
+                    }
+                }
+                if (out != null && out.trim().length() > 0) logger.finest(out);
+                if (err != null && err.trim().length() > 0) logger.finest(err);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.finest("Error executing rules: " + e.getMessage());
+        }
+    }
+    
     /*
      * TODO this should be handled with preference settings activity
      */
@@ -193,5 +329,12 @@ public class MainActivity extends Activity {
         if (!listenNonLocal){
             pref.edit().putBoolean(PreferenceUtils.proxyListenNonLocal, true).commit();
         }
+        
+        // we listen also for transparent flow 
+        boolean transparentProxy = pref.getBoolean(PreferenceUtils.proxyTransparentKey, false);
+        if (!transparentProxy){
+            pref.edit().putBoolean(PreferenceUtils.proxyTransparentKey, true).commit();
+        }
+        
     }
 }
