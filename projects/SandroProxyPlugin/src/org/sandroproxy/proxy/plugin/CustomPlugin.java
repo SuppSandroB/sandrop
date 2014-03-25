a10c87d12f1139bc77472395aa1f141d15513486
SandroB <supp.sandrob@gmail.com>
2014-03-25 11:43:37 +0100

    how to change wikipedia page title with custom plugin

--- projects/SandroProxyPlugin/src/org/sandroproxy/proxy/plugin/CustomPlugin.java
+++ projects/SandroProxyPlugin/src/org/sandroproxy/proxy/plugin/CustomPlugin.java
@@ -6,15 +6,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.sandrop.webscarab.httpclient.HTTPClient;
+import org.sandrop.webscarab.model.ConnectionDescriptor;
+import org.sandrop.webscarab.model.HttpUrl;
 import org.sandrop.webscarab.model.Request;
 import org.sandrop.webscarab.model.Response;
+import org.sandrop.webscarab.plugin.proxy.IClientResolver;
 import org.sandrop.webscarab.plugin.proxy.ProxyPlugin;
+import org.sandroproxy.utils.network.ClientResolver;
 
 import android.util.Log;
 
 public class CustomPlugin extends ProxyPlugin {
     
-    private static boolean LOGD = false;
+    private static boolean LOGD = true;
     private static String TAG = CustomPlugin.class.getName();
     private boolean _enabled = true;
     
@@ -52,9 +56,31 @@ public class CustomPlugin extends ProxyPlugin {
             if (_enabled) {
                 // adding some header to request
                 String setCookieHeader = "Set-Cookie";
+                boolean changeResponse = false;
                 request.addHeader("X-SandroProxyPlugin", "0.9.25");
                 // sending request to server to get response
+                ConnectionDescriptor cr = request.getConnectionDescriptor();
+                if (cr != null){
+                    if (LOGD) Log.d(TAG, "Connection data from: " + cr.getNamespace() + " " + cr.getId());
+                }
+                
+                HttpUrl reqUrl = request.getURL();
+                if (reqUrl != null && reqUrl.getHost() != null && reqUrl.getHost().equals("en.wikipedia.org") && reqUrl.getPath().equals("/wiki/Main_Page")){
+                    changeResponse = true; 
+                }
+                
                 Response response = _in.fetchResponse(request);
+                
+                // should we change response before goes to client
+                if (changeResponse && response != null && response.getStatus().equals("200")){
+                    byte[] responseContentByteArr = response.getContent();
+                    if (responseContentByteArr != null){
+                        String responseContentStr = new String (responseContentByteArr);
+                        String changedResponse = responseContentStr.replace("<title>Wikipedia, the free encyclopedia</title>", "<title>SandroProxy: Wikipedia, the free encyclopedia </title>");
+                        response.setContent(changedResponse.getBytes());
+                        if (LOGD) Log.d(TAG, "Response content modified by plugin");
+                    }
+                }
                 
                 // do we have any new cookies 
                 List<String> headerNames = Arrays.asList(response.getHeaderNames());
