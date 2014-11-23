d211b2f29f9429c8d6c6cca3e931dcc9b4ec4c99
SandroB <supp.sandrob@gmail.com>
2014-11-23 15:10:47 +0100

    adding iptables native build

--- projects/SandroProxyLib/jni/Android.mk
+++ projects/SandroProxyLib/jni/Android.mk
@@ -1,2 +1,17 @@
 LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles)
+ROOT_PATH := $(LOCAL_PATH)
+
+socket_dest_dir := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
+ 	socketdest \
+ 	))
+include $(socket_dest_dir)
+
+include $(CLEAR_VARS)
+# Iptables
+LOCAL_PATH := $(ROOT_PATH)
+ iptables_subdirs := $(addprefix $(LOCAL_PATH)/iptables/,$(addsuffix /Android.mk, \
+ 	iptables \
+ 	extensions \
+ 	libiptc \
+ 	))
+ include $(iptables_subdirs)
